0c9c91d4a415b5acfd234c915daf21be0de620b3
Gwendal Roulleau <gwendal.roulleau@gmail.com>
2022-09-10 07:50:42 +0200

    [signal] Initial contribution
    A binding to exchange messages with the Signal network.
    
    Signed-off-by: Gwendal Roulleau <gwendal.roulleau@gmail.com>

---
+++ bundles/org.openhab.binding.signal/src/main/java/org/openhab/binding/signal/internal/protocol/StateListener.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2010-2022 Contributors to the openHAB project
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package org.openhab.binding.signal.internal.protocol;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+/**
+ * Connectivity information interface
+ *
+ * @author Gwendal ROULLEAU - Initial contribution
+ *
+ */
+@NonNullByDefault
+public interface StateListener {
+
+    public static enum ConnectionState {
+        CONNECTING,
+        CONNECTED,
+        AUTH_FAILED,
+        DISCONNECTED;
+    };
+
+    public void newStateEvent(ConnectionState connectionState);
+
+    public void qrCodeToScan(String qrCode);
+}
