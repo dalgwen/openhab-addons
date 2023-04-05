ee441626493c17605fdb62f3c382b1dd5512eb75
Gwendal Roulleau <gwendal.roulleau@gmail.com>
2023-04-05 14:29:19 +0200

    [signal] Sending image capability
    Signed-off-by: Gwendal Roulleau <gwendal.roulleau@gmail.com>

--- bundles/org.openhab.binding.signal/src/main/java/org/openhab/binding/signal/internal/SignalBindingConstants.java
+++ bundles/org.openhab.binding.signal/src/main/java/org/openhab/binding/signal/internal/SignalBindingConstants.java
@@ -12,6 +12,8 @@
  */
 package org.openhab.binding.signal.internal;
 
+import java.util.Set;
+
 import org.eclipse.jdt.annotation.NonNullByDefault;
 import org.openhab.core.thing.ThingTypeUID;
 
@@ -25,6 +27,8 @@ import org.openhab.core.thing.ThingTypeUID;
 public class SignalBindingConstants {
 
     private static final String BINDING_ID = "signal";
+    public static final Set<String> PHOTO_EXTENSIONS = Set.of(".jpg", ".jpeg", ".png", ".gif", ".jpe", ".jif", ".jfif",
+            ".jfi", ".webp");
 
     // List of all Thing Type UIDs
     public static final ThingTypeUID SIGNALCONVERSATION_THING_TYPE = new ThingTypeUID(BINDING_ID, "signalconversation");
