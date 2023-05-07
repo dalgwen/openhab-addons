e5322c2c5239c1e65b70016e6392554b83090305
Miguel Álvarez <miguelwork92@gmail.com>
2023-05-07 12:59:07 +0200

    [habspeaker] Update deprecated sink implementation to lastest changes
    Signed-off-by: Miguel Álvarez <miguelwork92@gmail.com>

--- bundles/org.openhab.voice.habspeaker/web/src/utils/deprecated-audio-sink.ts
+++ bundles/org.openhab.voice.habspeaker/web/src/utils/deprecated-audio-sink.ts
@@ -6,54 +6,81 @@ const AUDIO_BUFFER_SIZE = 4096;
  */
 export class DeprecatedAudioSink extends AudioNodeSink {
     private audioCache: AudioCache;
+    private port: MessagePort;
+    private externalPort: MessagePort;
     private silence = new Float32Array(AUDIO_BUFFER_SIZE);
-    constructor(id: string, audioContext: AudioContext, channels: number, volume: number, listener: (playing: boolean) => void) {
+    private streaming: boolean = true;
+    private done: boolean = false;
+    constructor(id: string, audioContext: AudioContext, channels: number, volume: number) {
         const sinkProcessorNode = audioContext.createScriptProcessor(AUDIO_BUFFER_SIZE, 0, channels);
-        super(id, audioContext, sinkProcessorNode, channels, volume, listener);
+        super(id, audioContext, sinkProcessorNode, channels, volume);
         this.audioCache = new AudioCache();
         sinkProcessorNode.onaudioprocess = this.processAudio.bind(this);
+        const channel = new MessageChannel();
+        this.port = channel.port1;
+        this.port.onmessage = (ev) => this.handlePortMessage(ev.data);
+        this.externalPort = channel.port2;
     }
-    async setPort(port: MessagePort) {
-        port.onmessage = ev => {
-            if (ev.data instanceof Float32Array) {
-                this.audioCache.writeAudioData(ev.data);
-            }
-        };
+    async getPort() {
+        return this.externalPort;
+    }
+    private handlePortMessage(data: Float32Array | Float32Array[] | false) {
+        if (data instanceof Float32Array) {
+            this.audioCache.writeAudioData(data);
+        } else if (Array.isArray(data)) {
+            data.forEach(buffer => this.audioCache.writeAudioData(buffer));
+        } else if (data === false) {
+            this.streaming = false;
+        }
     }
     private processAudio(e: AudioProcessingEvent) {
-        if (this.audioCache.available(AUDIO_BUFFER_SIZE)) {
-            if (!this.playing) {
-                this.playing = true;
-                this.listener(true);
-            }
-            const audioData = this.audioCache.readAudioData(e.outputBuffer.length * this.channels);
-            const channelsData: Float32Array[] = [];
-            if (this.channels == 1) {
-                channelsData[0] = audioData;
-            } else {
-                const length = audioData.byteLength / audioData.BYTES_PER_ELEMENT;
-                for (let s = 0; s < length; s++) {
-                    // the channel index
-                    const c = s % this.channels;
-                    // the index inside the buffer channel
-                    const i = (s - c) / this.channels;
-                    const channelData = channelsData[c] = (channelsData[c] ?? new Float32Array(length / this.channels));
-                    channelData[i] = audioData[s];
+        const dataAvailable = this.audioCache.available(AUDIO_BUFFER_SIZE);
+        if (!dataAvailable) {
+            if (this.streaming) {
+                for (let c = 0; c < this.channels; c++) {
+                    e.outputBuffer.getChannelData(c).set(this.silence);
                 }
-            }
-            for (let c = 0; c < this.channels; c++) {
-                e.outputBuffer
-                    .getChannelData(c)
-                    .set(channelsData[c]);
-            }
-        } else {
-            if (this.playing) {
-                this.playing = false;
-                this.listener(false);
-            }
-            for (let c = 0; c < this.channels; c++) {
-                e.outputBuffer.getChannelData(c).set(this.silence);
+                return true;
+            } else {
+                if (this.audioCache.size() !== 0) {
+                    // send remaining audio
+                    const audioData = new Float32Array(AUDIO_BUFFER_SIZE);
+                    audioData.set(this.audioCache.readAudioData(this.audioCache.size()), 0);
+                    this.writeAudioSamples(audioData, e);
+                    return true;
+                }
+                if (!this.done) {
+                    this.done = true;
+                    // notify completion
+                    this.port.postMessage(false);
+                    this.port.close();
+                }
+                return false;
             }
         }
-    };
+        const audioData = this.audioCache.readAudioData(AUDIO_BUFFER_SIZE);
+        this.writeAudioSamples(audioData, e);
+        return true;
+    }
+    private writeAudioSamples(audioData: Float32Array, e: AudioProcessingEvent) {
+        const channelsData: Float32Array[] = [];
+        if (this.channels == 1) {
+            channelsData[0] = audioData;
+        } else {
+            const length = audioData.byteLength / audioData.BYTES_PER_ELEMENT;
+            for (let s = 0; s < length; s++) {
+                // the channel index
+                const c = s % this.channels;
+                // the index inside the buffer channel
+                const i = (s - c) / this.channels;
+                const channelData = channelsData[c] = (channelsData[c] ?? new Float32Array(length / this.channels));
+                channelData[i] = audioData[s];
+            }
+        }
+        for (let c = 0; c < this.channels; c++) {
+            e.outputBuffer
+                .getChannelData(c)
+                .set(channelsData[c]);
+        }
+    }
 }
