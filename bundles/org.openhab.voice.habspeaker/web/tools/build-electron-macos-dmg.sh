a4c6da19daf0885afb131aa6c2eb715a6ea452fd
Miguel Álvarez <miguelwork92@gmail.com>
2023-06-10 15:19:08 +0200

    [habspeaker] remove remaining librespot references
    Signed-off-by: Miguel Álvarez <miguelwork92@gmail.com>

--- bundles/org.openhab.voice.habspeaker/web/tools/build-electron-macos-dmg.sh
+++ bundles/org.openhab.voice.habspeaker/web/tools/build-electron-macos-dmg.sh
@@ -3,7 +3,6 @@ set -e
 cd "${0%/*}"
 cd ../
 HABSPEAKER_VERSION=$(cat package.json | jq -r '.version')
-LIBRESPOT_VERSION="FORK"
 TARGET_PLATFORM="${1:-amd64}"
 TARGET_PLATFORMS=("amd64", "arm64")
 if [[ ! "${TARGET_PLATFORMS[*]}" =~ "${TARGET_PLATFORM}" ]]; then
@@ -26,30 +25,6 @@ case $TARGET_PLATFORM in
     ELECTRON_PLATFORM_ARG=--x64
     ;;
 esac
-# Build Librespot
-LIBRESPOT_BINARY=librespot-src/target/$RUST_TARGET/release/librespot
-LIBRESPOT_LIBRARY=librespot-src/target/$RUST_TARGET/release/liblibrespot.rlib
-echostep "Builing Librespot binaries"
-if [[ ! -f $LIBRESPOT_BINARY || ! -f $LIBRESPOT_LIBRARY ]];then
-    if [ ! -f librespot-src/Cargo.toml ];then
-        echostep "Cloning Librespot $LIBRESPOT_VERSION source..."
-        # git -c advice.detachedHead=false clone --quiet --branch $LIBRESPOT_VERSION https://github.com/librespot-org/librespot.git librespot-src
-        # temporally use fork version to allow access token authentication
-        git -c advice.detachedHead=false clone --quiet --branch master https://github.com/GiviMAD/librespot.git librespot-src
-    else
-        echostep "Librespot source already exists, assuming desired version"
-    fi
-    echostep "Builing Librespot for $RUST_TARGET"
-    cd librespot-src
-    cargo build --target $RUST_TARGET --release
-    sleep 5
-    cd ../
-else
-    echostep "Librespot binaries for $RUST_TARGET found, skipping build"
-fi
-echostep "Copying Librespot binaries to appropriate directory"
-cp $LIBRESPOT_BINARY ./librespot/
-cp $LIBRESPOT_LIBRARY ./librespot/
 # Build habspekear dmg
 echostep "Installing HABSpeaker node deps"
 npm ci
--- bundles/org.openhab.voice.habspeaker/web/tools/build-electron-windows-exe.ps1
+++ bundles/org.openhab.voice.habspeaker/web/tools/build-electron-windows-exe.ps1
@@ -2,33 +2,7 @@ $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 cd $dir\..\
 $HABSPEAKER_VERSION = (Get-Content package.json) -join "`n" | ConvertFrom-Json | Select -ExpandProperty "version"
-$LIBRESPOT_VERSION = "FORK"
 Write-Host "Builing HABSpeaker $HABSPEAKER_VERSION electron exe for windown x86_64"
-if (-not(Test-Path -Path librespot-src\Cargo.toml -PathType Leaf)) {
-    Write-Host "Clonning Librespot $LIBRESPOT_VERSION"
-    # git -c advice.detachedHead=false clone --quiet --branch $LIBRESPOT_VERSION https://github.com/librespot-org/librespot.git librespot-src
-    # temporally use fork version to allow access token authentication
-    git -c advice.detachedHead=false clone --quiet --branch master https://github.com/GiviMAD/librespot.git librespot-src
-}
- else {
-     Write-Host "Skipping Librespot clone"
-}
-$RUST_TARGET="x86_64-pc-windows-msvc"
-$LIBRESPOT_BINARY="librespot-src\target\x86_64-pc-windows-msvc\release\librespot.exe"
-$LIBRESPOT_LIBRARY="librespot-src\target\x86_64-pc-windows-msvc\release\liblibrespot.rlib"
-if (-not(Test-Path -Path $LIBRESPOT_BINARY -PathType Leaf)) {
-    Write-Host "Builing Librespot"
-    cd librespot-src
-    cargo build --release --target $RUST_TARGET
-    cd ..\
-    sleep 5
-}
- else {
-     Write-Host "Skipping Librespot build"
-}
-Write-Host "Copying Librespot binaries"
-cp $LIBRESPOT_BINARY librespot\
-cp $LIBRESPOT_LIBRARY librespot\
 Write-Host "Build HABSpeaker electron exe"
 npm ci
 npm run build:electron
