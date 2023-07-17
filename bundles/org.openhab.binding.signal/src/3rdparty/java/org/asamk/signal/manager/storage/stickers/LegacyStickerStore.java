cd6ba384867196d293de46dda685683ca8d6fb82
Gwendal Roulleau <gwendal.roulleau@gmail.com>
2023-07-17 12:46:04 +0200

    [signal] signal-cli 0.11.11
    Signed-off-by: Gwendal Roulleau <gwendal.roulleau@gmail.com>

--- bundles/org.openhab.binding.signal/src/3rdparty/java/org/asamk/signal/manager/storage/stickers/LegacyStickerStore.java
+++ bundles/org.openhab.binding.signal/src/3rdparty/java/org/asamk/signal/manager/storage/stickers/LegacyStickerStore.java
@@ -32,7 +32,7 @@ public class LegacyStickerStore {
 
     public record Storage(List<Sticker> stickers) {
 
-        private record Sticker(String packId, String packKey, boolean installed) {
+        public record Sticker(String packId, String packKey, boolean installed) {
 
         }
     }
