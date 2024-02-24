2f4c72592d2587c376de1c3f49d4aaec720c6583
Gwendal Roulleau <gwendal.roulleau@gmail.com>
2024-02-24 02:34:32 +0100

    [signal] signal-cli 0.13

--- bundles/org.openhab.binding.signal/src/3rdparty/java/org/asamk/signal/manager/config/StagingConfig.java
+++ bundles/org.openhab.binding.signal/src/3rdparty/java/org/asamk/signal/manager/config/StagingConfig.java
@@ -20,28 +20,34 @@ import java.util.Optional;
 import okhttp3.Dns;
 import okhttp3.Interceptor;
 
+import static org.asamk.signal.manager.api.ServiceEnvironment.STAGING;
+
 class StagingConfig {
 
-    private final static byte[] UNIDENTIFIED_SENDER_TRUST_ROOT = Base64.getDecoder()
+    private static final byte[] UNIDENTIFIED_SENDER_TRUST_ROOT = Base64.getDecoder()
             .decode("BbqY1DzohE4NUZoVF+L18oUPrK3kILllLEJh2UnPSsEx");
-    private final static String CDSI_MRENCLAVE = "0f6fd79cdfdaa5b2e6337f534d3baf999318b0c462a7ac1f41297a3e4b424a57";
-    private final static String SVR2_MRENCLAVE = "a8a261420a6bb9b61aa25bf8a79e8bd20d7652531feb3381cbffd446d270be95";
+    private static final String CDSI_MRENCLAVE = "0f6fd79cdfdaa5b2e6337f534d3baf999318b0c462a7ac1f41297a3e4b424a57";
+    private static final String SVR2_MRENCLAVE = "acb1973aa0bbbd14b3b4e06f145497d948fd4a98efc500fcce363b3b743ec482";
+    private static final String SVR2_MRENCLAVE_DEPRECATED = "a8a261420a6bb9b61aa25bf8a79e8bd20d7652531feb3381cbffd446d270be95";
 
-    private final static String URL = "https://chat.staging.signal.org";
-    private final static String CDN_URL = "https://cdn-staging.signal.org";
-    private final static String CDN2_URL = "https://cdn2-staging.signal.org";
-    private final static String STORAGE_URL = "https://storage-staging.signal.org";
-    private final static String SIGNAL_CDSI_URL = "https://cdsi.staging.signal.org";
-    private final static String SIGNAL_SVR2_URL = "https://svr2.staging.signal.org";
-    private final static TrustStore TRUST_STORE = new WhisperTrustStore();
+    private static final String URL = "https://chat.staging.signal.org";
+    private static final String CDN_URL = "https://cdn-staging.signal.org";
+    private static final String CDN2_URL = "https://cdn2-staging.signal.org";
+    private static final String STORAGE_URL = "https://storage-staging.signal.org";
+    private static final String SIGNAL_CDSI_URL = "https://cdsi.staging.signal.org";
+    private static final String SIGNAL_SVR2_URL = "https://svr2.staging.signal.org";
+    private static final TrustStore TRUST_STORE = new WhisperTrustStore();
 
-    private final static Optional<Dns> dns = Optional.empty();
-    private final static Optional<SignalProxy> proxy = Optional.empty();
+    private static final Optional<Dns> dns = Optional.empty();
+    private static final Optional<SignalProxy> proxy = Optional.empty();
 
-    private final static byte[] zkGroupServerPublicParams = Base64.getDecoder()
-            .decode("ABSY21VckQcbSXVNCGRYJcfWHiAMZmpTtTELcDmxgdFbtp/bWsSxZdMKzfCp8rvIs8ocCU3B37fT3r4Mi5qAemeGeR2X+/YmOGR5ofui7tD5mDQfstAI9i+4WpMtIe8KC3wU5w3Inq3uNWVmoGtpKndsNfwJrCg0Hd9zmObhypUnSkfYn2ooMOOnBpfdanRtrvetZUayDMSC5iSRcXKpdlukrpzzsCIvEwjwQlJYVPOQPj4V0F4UXXBdHSLK05uoPBCQG8G9rYIGedYsClJXnbrgGYG3eMTG5hnx4X4ntARBgELuMWWUEEfSK0mjXg+/2lPmWcTZWR9nkqgQQP0tbzuiPm74H2wMO4u1Wafe+UwyIlIT9L7KLS19Aw8r4sPrXZSSsOZ6s7M1+rTJN0bI5CKY2PX29y5Ok3jSWufIKcgKOnWoP67d5b2du2ZVJjpjfibNIHbT/cegy/sBLoFwtHogVYUewANUAXIaMPyCLRArsKhfJ5wBtTminG/PAvuBdJ70Z/bXVPf8TVsR292zQ65xwvWTejROW6AZX6aqucUj");
-    private final static byte[] genericServerPublicParams = Base64.getDecoder()
+    private static final byte[] zkGroupServerPublicParams = Base64.getDecoder()
+            .decode("ABSY21VckQcbSXVNCGRYJcfWHiAMZmpTtTELcDmxgdFbtp/bWsSxZdMKzfCp8rvIs8ocCU3B37fT3r4Mi5qAemeGeR2X+/YmOGR5ofui7tD5mDQfstAI9i+4WpMtIe8KC3wU5w3Inq3uNWVmoGtpKndsNfwJrCg0Hd9zmObhypUnSkfYn2ooMOOnBpfdanRtrvetZUayDMSC5iSRcXKpdlukrpzzsCIvEwjwQlJYVPOQPj4V0F4UXXBdHSLK05uoPBCQG8G9rYIGedYsClJXnbrgGYG3eMTG5hnx4X4ntARBgELuMWWUEEfSK0mjXg+/2lPmWcTZWR9nkqgQQP0tbzuiPm74H2wMO4u1Wafe+UwyIlIT9L7KLS19Aw8r4sPrXZSSsOZ6s7M1+rTJN0bI5CKY2PX29y5Ok3jSWufIKcgKOnWoP67d5b2du2ZVJjpjfibNIHbT/cegy/sBLoFwtHogVYUewANUAXIaMPyCLRArsKhfJ5wBtTminG/PAvuBdJ70Z/bXVPf8TVsR292zQ65xwvWTejROW6AZX6aqucUjlENAErBme1YHmOSpU6tr6doJ66dPzVAWIanmO/5mgjNEDeK7DDqQdB1xd03HT2Qs2TxY3kCK8aAb/0iM0HQiXjxZ9HIgYhbtvGEnDKW5ILSUydqH/KBhW4Pb0jZWnqN/YgbWDKeJxnDbYcUob5ZY5Lt5ZCMKuaGUvCJRrCtuugSMaqjowCGRempsDdJEt+cMaalhZ6gczklJB/IbdwENW9KeVFPoFNFzhxWUIS5ML9riVYhAtE6JE5jX0xiHNVIIPthb458cfA8daR0nYfYAUKogQArm0iBezOO+mPk5vCM=");
+    private static final byte[] genericServerPublicParams = Base64.getDecoder()
             .decode("AHILOIrFPXX9laLbalbA9+L1CXpSbM/bTJXZGZiuyK1JaI6dK5FHHWL6tWxmHKYAZTSYmElmJ5z2A5YcirjO/yfoemE03FItyaf8W1fE4p14hzb5qnrmfXUSiAIVrhaXVwIwSzH6RL/+EO8jFIjJ/YfExfJ8aBl48CKHgu1+A6kWynhttonvWWx6h7924mIzW0Czj2ROuh4LwQyZypex4GuOPW8sgIT21KNZaafgg+KbV7XM1x1tF3XA17B4uGUaDbDw2O+nR1+U5p6qHPzmJ7ggFjSN6Utu+35dS1sS0P9N");
+
+    private static final byte[] backupServerPublicParams = Base64.getDecoder()
+            .decode("AHYrGb9IfugAAJiPKp+mdXUx+OL9zBolPYHYQz6GI1gWjpEu5me3zVNSvmYY4zWboZHif+HG1sDHSuvwFd0QszSwuSF4X4kRP3fJREdTZ5MCR0n55zUppTwfHRW2S4sdQ0JGz7YDQIJCufYSKh0pGNEHL6hv79Agrdnr4momr3oXdnkpVBIp3HWAQ6IbXQVSG18X36GaicI1vdT0UFmTwU2KTneluC2eyL9c5ff8PcmiS+YcLzh0OKYQXB5ZfQ06d6DiINvDQLy75zcfUOniLAj0lGJiHxGczin/RXisKSR8");
 
     static SignalServiceConfiguration createDefaultServiceConfiguration(
             final List<Interceptor> interceptors
@@ -58,7 +64,8 @@ class StagingConfig {
                 dns,
                 proxy,
                 zkGroupServerPublicParams,
-                genericServerPublicParams);
+                genericServerPublicParams,
+                backupServerPublicParams);
     }
 
     static ECPublicKey getUnidentifiedSenderTrustRoot() {
@@ -69,12 +76,12 @@ class StagingConfig {
         }
     }
 
-    static String getCdsiMrenclave() {
-        return CDSI_MRENCLAVE;
-    }
-
-    static String getSvr2Mrenclave() {
-        return SVR2_MRENCLAVE;
+    static ServiceEnvironmentConfig getServiceEnvironmentConfig(List<Interceptor> interceptors) {
+        return new ServiceEnvironmentConfig(STAGING,
+                createDefaultServiceConfiguration(interceptors),
+                getUnidentifiedSenderTrustRoot(),
+                CDSI_MRENCLAVE,
+                List.of(SVR2_MRENCLAVE, SVR2_MRENCLAVE_DEPRECATED));
     }
 
     private StagingConfig() {
